a858b66313d001db7122a5046eeed1d223a21941
saqibwaqar07-ui <saqibwaqar07@gmail.com>
2025-12-29 00:02:48 +0500

    Update script.js

--- web/script.js
+++ web/script.js
@@ -1,1 +1,8 @@
-console.log("JS file loaded");
+// Change header background when clicked
+document.addEventListener('DOMContentLoaded', () => {
+    const header = document.querySelector('header');
+    header.addEventListener('click', () => {
+        header.style.backgroundColor = '#00b4d8';
+        alert("Hello! You clicked the header.");
+    });
+});
